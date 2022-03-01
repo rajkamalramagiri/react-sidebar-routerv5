891e63c3e3df9134ce179999fa248b5ca694f4d8
“rajkamal” <v-surendra.raj@shutterfly.com>
2022-03-02 00:00:34 +0530

    sidebar left

--- src/App.js
+++ src/App.js
@@ -1,9 +1,66 @@
-import React from 'react'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+  Redirect
+} from "react-router-dom";
 
-function App() {
+import { AiFillHome, AiFillAppstore, AiFillCloud, AiFillMail } from 'react-icons/ai';
+
+
+import Home from './Home'
+import Bubblegum from './Bubblegum';
+import Email from './Email'
+import About from './About';
+import './App.css'
+
+export default function SidebarExample() {
   return (
-    <div>App</div>
-  )
-}
+    <Router>
+      <div className="container">
+        <div className="sidebar">
+          <ul>
+            <NavLink exact to="/" activeClassName="selected"
+            >
+              <li>
+                <AiFillHome />
+                <span>Home</span>
+              </li>
+            </NavLink>
+            <NavLink to="/about" activeClassName="selected">
+              <li>
+                <AiFillAppstore />
+                <span>About</span>
+              </li>
+            </NavLink>
+            <NavLink activeClassName="selected" to="/bubblegum">
+              <li>
+                <AiFillCloud />
+                <span>Bubblegum</span>
+              </li>
+            </NavLink>
+            <NavLink activeClassName="selected" to="/email">
+              <li>
+                <AiFillMail />
+                <span>Email</span>
+              </li>
+            </NavLink>
+          </ul>
+        </div>
 
-export default App
+        <div>
+          <Switch>
+            <Route path="/about" component={About} />
+            <Route path="/bubblegum" component={Bubblegum} />
+            <Route path="/email" component={Email} />
+            <Route exact path="/" component={Home} />
+            <Redirect exact from="*" to="/" />
+          </Switch>
+
+        </div>
+      </div>
+    </Router>
+  );
+}
---
+++ src/Bubblegum.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Bubblegum() {
+    return (
+        <div>Bubblegum</div>
+    )
+}
+
+export default Bubblegum
